82ecf3f8acb81f3d64d1067a2d9183582efd5dea
Gaël Poupard <gael>
2013-02-28 17:55:48 +0100

    Amélioration de la hiérarchie des templates
    Une confusion existait entre la page d'index du blog et la page
    d'accueil statique : afin de clarifier ça j'ai ajouté le front-page.php
    pour accueillir la page d'accueil statique, et home.php pour l'index du
    blog ( ce fichier est optionnel puisqu'en son absence index.php sera
    utilisé : ces deux fichiers sont pour le moment identiques mais pourront
    être différenciés en cas de besoin ).
    
    @see http://codex.wordpress.org/images/1/18/Template_Hierarchy.png

---
+++ front-page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Accueil
+ * Page d'accueil
+ * @author Gaël Poupard
+ * @link www.ffoodd.fr
+ *
+ * En savoir plus : http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package 	WordPress
+ * @subpackage 	ffeeeedd
+ * @since 		ffeeeedd 1.0
+ */
+get_header(); ?>
+
+    <?php get_template_part( 'loop', 'diaporama' ); ?>
+    
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        <article itemscope itemtype="http://schema.org/Article" role="article" class="col">
+            <h2 itemprop="name"><?php the_title(); ?></h2>
+            <div itemprop="articleBody"><?php the_content(); ?></div>
+        </article>
+    <?php endwhile; ?>  
+
+    <?php get_sidebar( 'home' ); ?>
+
+<?php get_footer(); ?>
--- home.php
+++ home.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Template Name: Accueil
- * Page d'accueil
+ * Index du blog
  * @author Gaël Poupard
  * @link www.ffoodd.fr
  *
@@ -12,16 +11,40 @@
  * @since 		ffeeeedd 1.0
  */
 get_header(); ?>
-
-    <?php get_template_part( 'loop', 'diaporama' ); ?>
     
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-        <article itemscope itemtype="http://schema.org/Article" role="article" class="col">
-            <h2 itemprop="name"><?php the_title(); ?></h2>
-            <div itemprop="articleBody"><?php the_content(); ?></div>
-        </article>
-    <?php endwhile; ?>  
+<?php if ( have_posts() ): ?>
 
-    <?php get_sidebar( 'home' ); ?>
+    <h2>Articles récents</h2>
+
+    <ol>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <li class="mb2">
+                <article itemscope itemtype="http://schema.org/Article">
+                    <h3 itemprop="name"><a href="<?php esc_url( the_permalink() ); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h3>
+                    <time datetime="<?php the_time( 'j-F-Y' ); ?>" pubdate itemprop="datePublished"><?php the_date(); ?></time>
+                    <?php $excerpt = get_the_excerpt() ?>
+                    <p itemprop="description"><?php echo $excerpt ?></p>
+                </article>
+            </li>
+        <?php endwhile; ?>
+    </ol>
+
+    <?php
+        global $wp_query;        
+        $big = 999999999;        
+        echo paginate_links( array(
+            'base'         => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+            'format'       => '?paged=%#%',
+            'current'      => max( 1, get_query_var('paged') ),
+            'total'        => $wp_query->max_num_pages,
+            'prev_text'    => __('&larr; Précédent'),
+            'next_text'    => __('Suivant &rarr;'),
+            'type'         => 'list'
+        ) );
+    ?>
+
+<?php else: ?>
+    <h2>Il n'y a aucun article à afficher.</h2>
+<?php endif; ?>
 
 <?php get_footer(); ?>
