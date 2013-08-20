2cdfb209a4f4bb240f3e3c340e6b2b135fb85732
Gaël Poupard <gael>
2013-08-20 12:24:40 +0200

    MAJ WP 3.6 - rôle search
    Il manquait le rôle ARIA "search" sur le formulaire de recherche.

--- searchform.php
+++ searchform.php
@@ -10,7 +10,7 @@
  */
 ?>
 
-  <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label for="s" aria-hidden="true"><?php _e( 'Search', 'ffeeeedd' ); ?></label>
     <input id="s" type="search" name="s" placeholder="<?php esc_attr_e( 'Search', 'ffeeeedd' ); ?>" />
     <input type="submit" name="submit" value="<?php esc_attr_e( 'Search', 'ffeeeedd' ); ?>" />
