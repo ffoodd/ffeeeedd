ad094cba465a2794db138c782b370b581597c5d5
Gaël Poupard <gael.poupard@gmail.com>
2013-11-12 17:57:42 +0100

    Amélioration de la gestion des images
    Les images décoratives ne sont plus manipulées (la manipulation
    n'apportait rien) par contre l'intervention sur les [caption] est
    ajoutée par défaut à ffeeeedd.

--- ffeeeedd__functions--front.php
+++ ffeeeedd__functions--front.php
@@ -44,7 +44,7 @@
     -- Ajoute un <link rel="canonical" /> si le champ est rempli
   == Ajout des métas Image dans le <head>
   == Amélioration de la recherche
-  == Les <img> sont contenues dans des <figcaption>
+  == Remplace le code généré par [caption]
 */
 
 
@@ -845,16 +845,38 @@
   }
 
 
-  /* == @section Les <img> sont contenues dans des <figcaption> ==================== */
+  /* == @section Remplace le code généré par [caption] ==================== */
   /**
-   * @note : Le contenu est filtré pour remplacer le conteneur automatique <p> autour des images par une balise plus sémantique <figure>. Astuce trouvée sur Reverie.
+   * @note : Le contenu est filtré pour remplacer le html généré pour les caption par du html5 sémantique. Astuce trouvée sur Reverie.
    * @author : Zhen Huang
    * @see : http://themefortress.com/reverie/
    * @see : https://github.com/milohuang/reverie/blob/master/lib/clean.php
+   * @note : On y ajoute les microdonnées qui vont bien.
+   * @author : Joost Kiens ( @joostkiens )
+   * @see : https://gist.github.com/JoostKiens/4477366
+   * @note : Et j'y ajoute les roles et attributs Aria nécessaires
+   * @see : http://www.kloh.ch/craw2013-html5-aria-et-accessibilite-web-479
    */
-
-  add_filter( 'the_content', 'ffeeeedd__images', 30 );
-  function ffeeeedd__images( $img ) {
-    $img = preg_replace( '/<p>\\s*?(<a .*?><img.*?><\\/a>|<img.*?>)?\\s*<\\/p>/s', '<figure role="presentation">$1</figure>', $img );
-    return $img;
+  add_filter( 'img_caption_shortcode', 'ffeeeedd__caption', 10, 3 );
+  function ffeeeedd__caption( $output, $attr, $content ) {
+    if ( is_feed() ) {
+      return $output;
+    }
+    $defaults = array(
+      'id' => '',
+      'align' => 'alignnone',
+      'width' => '',
+      'caption' => ''
+    );
+    $attr = shortcode_atts( $defaults, $attr );
+    if ( 1 > (int) $attr['width'] || empty( $attr['caption'] ) ) {
+      return $content;
+    }
+    $content = str_replace( '<img', '<img id="' . $attr['id'] . '" itemprop="contentURL" aria-describedby="wp-caption--' . $attr['id'] . '"', $content );
+    $attributes = ' class="wp-caption inbl ' . esc_attr( $attr['align'] ) . '"';
+    $output = '<figure' . $attributes .' role="group" itemscope itemtype="http://schema.org/ImageObject">';
+    $output .= do_shortcode( $content );
+    $output .= '<figcaption class="wp-caption-text pt1 small" id="wp-caption--' . $attr['id'] . '" itemprop="description">' . $attr['caption'] . '</figcaption>';
+    $output .= '</figure>';
+    return $output;
   }
