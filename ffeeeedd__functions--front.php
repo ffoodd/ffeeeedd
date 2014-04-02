ee435a3a58fbc6d778765dbeead9bd9c79f50643
Gaël Poupard <gael.poupard@gmail.com>
2014-04-02 23:30:22 +0200

    Liens inutiles
    À enlever au cas ou :-)

--- ffeeeedd__functions--front.php
+++ ffeeeedd__functions--front.php
@@ -94,7 +94,11 @@
   remove_action( 'wp_head', 'wp_dlmp_l10n_style' );
   remove_action( 'wp_head', 'rsd_link' );
   remove_action( 'wp_head', 'wlwmanifest_link' );
-  remove_action('wp_head', 'feed_links', 2);
+  remove_action( 'wp_head', 'index_rel_link' );
+  remove_action( 'wp_head', 'parent_post_rel_link' );
+  remove_action( 'wp_head', 'start_post_rel_link' );
+  remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head' );
+  remove_action( 'wp_head', 'feed_links', 2);
   remove_action( 'wp_head', 'feed_links_extra', 3 );
 
   /*-- @subsection Retire les attributs title inutiles sur les liens générés par WordPress */
